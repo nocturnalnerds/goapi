48455de2a34b74de7d53ddff60e60eb77b7f950e
Williams Sandjaya <133097737+nocturnalnerds@users.noreply.github.com>
2025-08-20 01:15:01 +0700

    taik

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -13,4 +13,11 @@ func main(){
 	log.SetReportCaller(true)
 	var r *chi.Mux = chi.NewRouter()
 	handlers.Handler(r)
+
+	fmt.Println("Starting GO API Service");
+
+	err := http.ListenAndServe("localhost:8000", r)
+	if err != nil {
+		log.Error(err)
+	}
 }
